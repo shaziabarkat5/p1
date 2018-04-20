c8b2e055a229430d4b518bb22e2e3dd2a7edc4c3
shaziabarkat5 <38329467+shaziabarkat5@users.noreply.github.com>
2018-04-20 17:10:07 +0530

    Add files via upload

---
+++ g2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a no :");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("no is even");
+    }
+    else{
+        printf("no is odd");
+    }
+    return 0;
+}
